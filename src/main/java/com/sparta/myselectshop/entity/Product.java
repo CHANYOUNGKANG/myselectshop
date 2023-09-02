bd56b2159e0102dbadd92b363f79bd61721948fb
Chanyoung Kang <chanyoungcplus@gmail.com>
2023-09-03 06:36:31 +0900

    상품 폴더 설꼐

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
@@ -37,6 +40,9 @@ public class Product extends Timestamped {
     @ManyToOne(fetch = FetchType.LAZY)// 다대일 관계 설정 // 지연로딩// User 객체를 참조하는 필드
     @JoinColumn(name = "user_id", nullable = false)// 외래키 이름 지정 // null 허용 안함// User 객체를 참조하는 필드
     private User user;// User 객체를 참조하는 필드
+
+    @OneToMany(mappedBy = "product")
+    private List<ProductFolder> productFolderList = new ArrayList<>();
 
     public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
---
+++ src/main/java/com/sparta/myselectshop/repository/ProductFolerRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.myselectshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolerRepository extends JpaRepository<ProductFolerRepository, Long> {
+
+}
