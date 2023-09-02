508e799941ea21c0fb4cd3133fa9a36ea9a18a65
Chanyoung Kang <chanyoungcplus@gmail.com>
2023-09-03 05:20:04 +0900

    Scheduler 기능구현

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,6 +3,9 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling//스케줄링 활성화
 
 @EnableJpaAuditing//JPA Auditing 활성화
 @SpringBootApplication
